59fa90f88138352eb2a768fb9377f16b4546c4fb
nikananu <n_nanuashvili@cu.edu.ge>
2025-01-31 13:44:29 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string text)
+    {
+        // გადააქციე ტექსტი პატარა ასოებად
+        text = text.ToLower();
+
+        // შევადაროთ ტექსტი მის საპირისპირო ვერსიას
+        string reversedText = new string(text.Reverse().ToArray());
+
+        return text == reversedText;
+    }
+
+    static void Main()
+    {
+        string input = "racecar";
+        bool result = IsPalindrome(input);
+        Console.WriteLine($"Is Palindrome: {result}");
+    }
+}
